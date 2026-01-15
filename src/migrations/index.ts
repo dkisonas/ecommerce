a927f8bfe8618c0bbfec855dfac31bc58011901b
Dominykas Kišonas <dominykas.kisonas@inventi.lt>
2026-01-15 14:50:53 +0200

    add migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260115_125034 from './20260115_125034';
+
+export const migrations = [
+  {
+    up: migration_20260115_125034.up,
+    down: migration_20260115_125034.down,
+    name: '20260115_125034'
+  },
+];
